61318253e11ba7f5ce4e30d509b0ddc5a020f5b1
ViperSniper0501 <michaelabrenner@gmail.com>
2023-04-30 13:52:19 -0400

    Initial commit

---
+++ moss-client/src/main.rs
@@ -0,0 +1,16 @@
+
+
+
+/// Attempts to form connection with server.
+/// Connection Fails:
+///     Exit and report failure
+/// Connection Succeeds: 
+///     Deserialize data
+///     Data should contain the instructions for the checks
+/// Main loop:
+///     - Perform Checks
+///     - Post checks to server
+///     - repeat
+fn main() {
+
+}
