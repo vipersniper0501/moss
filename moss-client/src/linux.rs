e7cfe3ff55b22e1d081a2b383283fef3dd0205dd
ViperSniper0501 <michaelabrenner@gmail.com>
2023-06-19 22:20:43 -0400

    worked on adding the find_file function in src/linux.rs

--- moss-client/src/linux.rs
+++ moss-client/src/linux.rs
@@ -1,11 +1,26 @@
 //! Linux functions
 
+use std::fs;
+use std::path::Path;
+
 fn perform_checks() {
 
 }
 
-fn detect_file() {
+fn detect_file(path: &str) -> Result<bool, std::io::Error> {
+    let path = Path::new(path);
 
+    // Note: This will fail if the current user does not have permission to read
+    // the file!
+    if !path.is_file() {
+        return Err(std::io::Error::new(
+                std::io::ErrorKind::NotFound,
+                "File not found."
+        ));
+    }
+
+
+    Ok(true)
 }
 
 fn list_users() {
