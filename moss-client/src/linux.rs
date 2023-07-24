251913e3371b9abede727de6cfc5afb87b5719b8
ViperSniper0501 <michaelabrenner@gmail.com>
2023-07-24 16:35:26 -0400

    Completed linux list_users function and created a simple test that should work accross machines in moss-client/src/linux.rs

--- moss-client/src/linux.rs
+++ moss-client/src/linux.rs
@@ -1,6 +1,6 @@
 //! Linux functions
 
-use std::{path::Path, fs::{self, Metadata}};
+use std::fs;
 
 use crate::structures::*;
 
@@ -21,19 +21,8 @@ pub fn perform_checks(config_data: &MossData) -> MossResults {
 
 // fn detect_file(path: &str) -> Result<bool, std::io::Error> {
 fn detect_file(path: &str) -> Result<bool, std::io::Error> {
-    // let path = Path::new(path);
 
     let path = fs::metadata(path)?;
-    // Note: This will fail if the current user does not have permission to read
-    // the file!
-    // if !path.is_file() {
-        // return Err(std::io::Error::new(
-                // std::io::ErrorKind::NotFound,
-                // "File not found."
-        // ));
-    // }
-
-    // Ok(true)
 
     return Ok(path.is_file());
 }
@@ -76,11 +65,52 @@ fn detect_invalid_files(config_data: &MossData, mut results: MossResults) -> Mos
             );
     }
 
+    // temp
+    // list_users();
     return results;
 }
 
-fn _list_users() {
+fn list_users() -> Vec<LinuxUserData> {
+    let mut users: Vec<LinuxUserData> = Vec::new();
 
+    // Read the output of the cat of the file
+    let output = std::process::Command::new("cat")
+        .arg("/etc/passwd")
+        .output()
+        .expect("Failed to execute cat command on /etc/passwd");
+
+    let output = String::from_utf8_lossy(&output.stdout);
+    let output: Vec<&str> = output.split('\n').collect();
+
+    let output_iter = output.iter();
+
+    // Build the user
+    for s in output_iter {
+        let data: Vec<&str> = s.split(':').collect();
+
+        // Fixes bug with the output.split that leaves a trailing entry. 
+        // Print out output if you don't believe me.
+        if data.len() < 7 {
+            break;
+        }
+
+        let mut new_user: LinuxUserData = LinuxUserData::new();
+        new_user.name = data[0].to_string();
+        new_user.has_password = match data[1] {
+            "x" => true,
+            _ => false,
+        };
+        new_user.uid = data[2].parse::<i32>()
+            .expect("Failed to convert uid to i32");
+        new_user.gid = data[3].parse::<i32>()
+            .expect("Failed to convert gid to i32");
+        new_user.home_dir = data[5].to_string();
+        new_user.shell = data[6].to_string();
+
+        users.push(new_user);
+    }
+
+    return users;
 }
 
 fn _detect_valid_users() {
@@ -94,3 +124,17 @@ fn _detect_invalid_users() {
 
 // Tests Coming Soon!
 
+#[cfg(test)]
+mod tests {
+    use crate::{structures::*,
+        linux::list_users};
+
+    #[test]
+    fn test_list_users() {
+        let data: Vec<LinuxUserData> = list_users();
+        assert_eq!(data[0].name, "root");
+        assert_eq!(data[0].has_password, true);
+        assert_eq!(data[0].uid, 0);
+    }
+    
+}
