fb1de71ea6a33dec4633aee5618eacc274169f66
ViperSniper0501 <michaelabrenner@gmail.com>
2023-09-01 13:34:10 -0400

    Setup basic moss admin site in moss-frontend

--- moss-server/moss-frontend/src/main.rs
+++ moss-server/moss-frontend/src/main.rs
@@ -6,7 +6,38 @@ fn app() -> Html {
     
 
     html! {
-        <h1>{"Moss"}</h1>
+        <>
+            <div class="nav-horizontal">
+                <ul>
+                    <li><a class={"active"} href={"/"}>{"Home"}</a></li>
+                    <li><a href={"/"}>{"Configurations"}</a></li>
+                    <li style={"float: right; display: block; \
+                                color: white; text-align: center; \
+                                padding: 14px 16px; text-decoration: none;"}>
+                                {"Moss Admin Dashboard"}</li>
+                </ul>
+            </div>
+            <div class={"content"}>
+                <h1>{"Moss"}</h1>
+                <div class={"cards-column"}>
+                    <div class={"card"}>
+                        <p>{"Card 1"}</p>
+                    </div>
+                    <div class={"card"}>
+                        <p>{"Card 2"}</p>
+                    </div>
+                    <div class={"card"}>
+                        <p>{"Card 3"}</p>
+                    </div>
+
+                </div>
+
+                <div class={"teams-column"}>
+
+                    <p>{"Test"}</p>
+                </div>
+            </div>
+        </>
     }
 }
 
